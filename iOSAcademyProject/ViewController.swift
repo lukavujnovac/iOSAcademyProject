f32e20b5dd09df8ef4d1555d02d1a61d48562efe
Luka <vujnovacluka89@gmail.com>
2022-05-21 11:34:30 +0200

    initial project setup

---
+++ iOSAcademyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAcademyProject
+//
+//  Created by Luka Vujnovac on 21.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
